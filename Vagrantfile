3c78fd2b8864d0f8701d1900dbfa1b347ac9bca1
tsunagun <tsunagu.honma@gmail.com>
2015-01-23 19:28:53 +0900

    Virtuosoのインストールスクリプトを修正

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@
 
 box = "ubuntu/trusty64"
 vm_name = "vagrant_template"
-ip_address = "192.168.33.1"
+ip_address = "192.168.33.101"
 
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
@@ -20,7 +20,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     machine.vm.network "private_network", ip: ip_address
   end
 
-  #config.vm.provision :shell, :path => "init.sh"
+  config.vm.provision :shell, :path => "init.sh"
 
   # Enable provisioning with chef solo, specifying a cookbooks path, roles
   # path, and data_bags path (all relative to this Vagrantfile), and adding
--- init.sh
+++ init.sh
@@ -5,19 +5,29 @@ sudo aptitude safe-upgrade -y
 
 sudo ufw default DENY
 sudo ufw limit ssh
-sudo ufw enable
+sudo ufw allow 8890
+yes | sudo ufw enable
 
 sudo sed -i -e "s/^PasswordAuthentication.*/PasswordAuthentication no/g" /etc/ssh/sshd_config
 sudo service ssh restart
 
-sudo aptitude install -y build-essential autoconf libtool flex bison gperf gawk git-core vim
-git clone https://github.com/openlink/virtuoso-opensource.git
-cd virtuoso-opensource
-export CFLAGS="-O2 -m64"  # 64bit版としてビルドするための設定
+sudo aptitude install -y build-essential autoconf automake libssl-dev libtool flex bison gperf gawk git-core vim
+cp -r /vagrant/virtuoso-opensource ~/
+cd ~/virtuoso-opensource
+git pull
+git checkout stable/7
 ./autogen.sh
-./configure --with-layout=debian
+./configure --with-layout=debian --program-transform-name="s/isql/isql-v/" CFLAGS="-O2 -m64"
 make
 sudo make install
-# sudo vim /var/lib/virtuoso/db/virtuoso.ini  #設定ファイルの編集
-# サーバの起動は virtuoso-t -c /var/lib/virtuoso/db/virtuoso.ini
-#!/usr/bin/env bash
+sudo chown -R vagrant /var/lib/virtuoso/db
+
+cat <<EOS
+
+設定ファイルの場所
+  /var/lib/virtuoso/db/virtuoso.ini
+
+起動コマンド
+  virtuoso-t -c /var/lib/virtuoso/db/virtuoso.ini
+
+EOS
